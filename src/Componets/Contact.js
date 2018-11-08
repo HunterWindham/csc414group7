49dba6368fbb6837056bb10482d5949e30163d32
Pujan Paudel <ppaudel@seahawk.st.usm.edu>
2018-11-08 15:30:38 -0600

    Nikhil Uploaded Contacts File

---
+++ src/Componets/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Contact extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Contact Information !! </h1>
+        <div id="ContactFields">
+     <p>Name: Richard McKissack                      </p>
+     <p>Phone:601-447-0000                     </p>
+     <p>Email: Richard.McKissack@usm.edu                    </p>
+     <p>Address: Tec 251, Bobby Chain Technology Building,USM                  </p>
+        </div>
+      </div>
+        );
+  }
+}
